33536e9dd6a948612ec1dc2fec91bd0b31710a5f
ERAY AVCI <erayavcioffical@gmail.com>
2021-01-28 15:04:25 +0300

    Add project files.

---
+++ SQLQuery2.sql
@@ -0,0 +1,20 @@
+﻿--Select
+Select ContactName Adi,CompanyName Şirketadi,City Sehir from Customers
+
+Select * from Products where categoryId=1 or categoryId=3
+
+select * from Products where categoryId=1 and UnitPrice>=10
+
+select * from Products order by UnitPrice desc ---ascendig ---descending
+
+select count(*) from Products  ---bütün product kolonunda kaç data var
+
+select count (*) Adet from Products where CategoryID=2
+
+select categoryID,count(*) from products where UnitPrice>20 group by CategoryID having count(*)<10
+
+select *
+from Products inner join Categories on Products.CategoryID = Categories.CategoryID
+
+
+
